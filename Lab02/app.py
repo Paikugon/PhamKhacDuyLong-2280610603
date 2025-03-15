c54ed4d3ef342294a22245a6a6fbc855336eff7c
Phạm Khắc Duy Long <longphamkhacduy@gmail.com>
2025-03-15 14:06:24 +0700

    For my stupid OCD brain

--- Lab02/app.py
+++ Lab02/app.py
@@ -38,70 +38,70 @@ def Caesar_encrypt():
     plaintext = request.form['plaintext']
     key = int(request.form['key'])
     ciphertext = CaesarCipher.encrypt(plaintext, key)  
-    return jsonify({'ciphertext': ciphertext})
+    return f"<pre>text: {plaintext} <br/>key: {key}<br/>ciphertext: {ciphertext}</pre>"
 
 @app.route('/caesar/decrypt', methods=['POST'])
 def Caesar_decrypt():
     ciphertext = request.form['ciphertext']
     key = int(request.form['key'])
     plaintext = CaesarCipher.decrypt(ciphertext, key)
-    return jsonify({'plaintext': plaintext})
+    return f"<pre>ciphertext: {ciphertext} <br/>key: {key}<br/>plaintext: {plaintext}</pre>"
 
 @app.route('/vigenere/encrypt', methods=['POST'])
 def Vigenere_encrypt():
     plaintext = request.form['plaintext']
     key = request.form['key']
     ciphertext = VigenereCipher.encrypt(plaintext, key)
-    return jsonify({'ciphertext': ciphertext})
+    return f"<pre>text: {plaintext} <br/>key: {key}<br/>ciphertext: {ciphertext}</pre>"
 
 @app.route('/vigenere/decrypt', methods=['POST'])
 def Vigenere_decrypt():
     ciphertext = request.form['ciphertext']
     key = request.form['key']
     plaintext = VigenereCipher.decrypt(ciphertext, key)
-    return jsonify({'plaintext': plaintext})
+    return f"<pre>ciphertext: {ciphertext} <br/>key: {key}<br/>plaintext: {plaintext}</pre>"
 
 @app.route('/playfair/encrypt', methods=['POST'])
 def PlayFair_encrypt():
     plaintext = request.form['plaintext']
     key = request.form['key']
     ciphertext = PlayFairCipher.read(plaintext, key, 1)
-    return jsonify({'ciphertext': ciphertext})
+    return f"<pre>text: {plaintext} <br/>key: {key}<br/>ciphertext: {ciphertext}</pre>"
 
 @app.route('/playfair/decrypt', methods=['POST'])
 def PlayFair_decrypt():
     ciphertext = request.form['ciphertext']
     key = request.form['key']
     plaintext = PlayFairCipher.read(ciphertext, key, -1)
-    return jsonify({'plaintext': plaintext})
+    return f"<pre>ciphertext: {ciphertext} <br/>key: {key}<br/>plaintext: {plaintext}</pre>"
 
 @app.route('/railfence/encrypt', methods=['POST'])
 def RailFence_encrypt():
     plaintext = request.form['plaintext']
     key = int(request.form['key'])
     ciphertext = RailFenceCipher.encrypt(plaintext, key)
-    return jsonify({'ciphertext': ciphertext})
+    return f"<pre>text: {plaintext} <br/>key: {key}<br/>ciphertext: {ciphertext}</pre>"
 
 @app.route('/railfence/decrypt', methods=['POST'])
 def RailFence_decrypt():
     ciphertext = request.form['ciphertext']
     key = int(request.form['key'])
     plaintext = RailFenceCipher.decrypt(ciphertext, key)
-    return jsonify({'plaintext': plaintext})
+    return f"<pre>ciphertext: {ciphertext} <br/>key: {key}<br/>plaintext: {plaintext}</pre>"
 
 @app.route('/transposition/encrypt', methods=['POST'])
 def Transposition_encrypt():
     plaintext = request.form['plaintext']
     key = int(request.form['key'])
     ciphertext = TranspositionCipher.encrypt(plaintext, key)
-    return jsonify({'ciphertext': ciphertext})
+    return f"<pre>text: {plaintext} <br/>key: {key}<br/>ciphertext: {ciphertext}</pre>"
 
 @app.route('/transposition/decrypt', methods=['POST'])
 def Transposition_decrypt():
     ciphertext = request.form['ciphertext']
     key = int(request.form['key'])
     plaintext = TranspositionCipher.decrypt(ciphertext, key)
-    return jsonify({'plaintext': plaintext})
+    return f"<pre>ciphertext: {ciphertext} <br/>key: {key}<br/>plaintext: {plaintext}</pre>"
 
 if __name__ == '__main__':
     app.run(debug=True);
